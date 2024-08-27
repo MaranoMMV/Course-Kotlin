03f558c1f8cedf0fcba7d4270be30a370bc6ad7b
Matheus Marano de Vasconcelos <matheus.marano@pcv.local>
2024-08-27 12:49:51 -0300

    Meu primeiro projeto android

--- 2 - Mais sobre Parametros/src/Main.kt
+++ 2 - Mais sobre Parametros/src/Main.kt
@@ -16,3 +16,7 @@ fun media( vararg notas: Float): Float{
     }
     return (soma / notas.size)
 }
+
+fun salary(salary: Float, hourlyRate: Float = 200.0f, workedTime: Float) : Float {
+    return salary / hourlyRate * workedTime
+}
